a7390bff20180039cbebed7619a2b3beee1801e1
smw1414 <wsm147@outlook.com>
2017-09-19 18:29:11 +0800

    lncRNA local run script

---
+++ lncrna_local_run_deg.sh
@@ -0,0 +1,11 @@
+rawread_count=$1
+contab=$2
+rm -rf output
+mkdir -p output
+./deseq2.r -r $rawread_count -f $contab -a gencodev25 -c -m lnc -a gencodev25 -c 4
+./deseq2_annotation.r -i output/DEGs_lncRNA.txt -o output/DEGs_lncRNA_ann.txt
+cp $contab count_matrix_conditon.txt
+
+
+
+
