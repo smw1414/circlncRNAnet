bc9387627ff8017877aec37907d53203b3967d38
smw1414 <wsm147@outlook.com>
2017-09-20 11:58:27 +0800

    modification

--- lncrna_local_run_cor.sh
+++ lncrna_local_run_cor.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 genes=$1
 ./correlation.r -n output/norm_readstable.txt -f count_matrix_conditon.txt -q $genes -a gencodev25 -s symbol -m lnc
 ./coexp_pairs_annotation.r -i output/linc_coexp_pairs.txt -o output/linc_coexp_pairs_ann.txt
