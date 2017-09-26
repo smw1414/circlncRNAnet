023dac45d0a16068d57a704618cf1abd0f2bb97e
smw1414 <wsm147@outlook.com>
2017-09-26 11:27:06 +0800

    add comments

--- deseq2_annotation.r
+++ deseq2_annotation.r
@@ -25,11 +25,14 @@ if ( is.null(opt$output ) ) { opt$output = "output.txt" }
 
 library("data.table")
 
+# loading annotation
 annotation=readRDS("dbNSFP3.2_gene_lite.rds")
 deg=fread(opt$input,header=T,sep="\t")
 genelist=paste(deg[abs(log2FoldChange)>=1&pvalue<=0.05,]$gene,collapse=",")
 write.table(genelist,"output/genelist.txt",quote=F,col.names=F,row.names=F)
 output=merge(deg,annotation,all.x=T,by.x="gene",by.y="Gene_name")
+
+# merge gene with annotation
 write.table(output,opt$output,quote=F,sep="\t",row.names=F)
 #system(paste0("/share/apps/lancer2/tabulate_lncRNA_RPB_r1.R -q ",deg[abs(log2FoldChange)>=1&pvalue<=0.05,]$gene[1]))
 #system(paste0("/share/apps/lancer2/correlation_20161206.r"," -n output/norm_readstable.txt -f count_matrix_conditon.txt -a gencodev25 -s symbol -m lnc -q ",genelist))
